c14b068d36a9b82a756484c4468f48d5895e061a
Christos <chris.paraskeva10@gmail.com>
2017-01-19 15:10:36 +0000

    Challenge 6 completed

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -8,4 +8,40 @@
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  string_ary = string.split('')
+  
+  if (return_odds == true)
+    string_ary.select.with_index { |_, i| i.odd? }.join('')
+  else
+    string_ary.select.with_index { |_, i| i.even? }.join('')
+  end
 end
+  
+  
+  
+  
+  
+  
+  
+  
+  # ary = Array.new
+#   i = 0
+#   c = 0
+#   string_new = string.split('')
+#   if (return_odds == true)
+#     ary << string_new[(i+5)] = (string_new[i] + 2)
+#
+ #
+#   end
+# end
+  
+  
+  
+  
+  # n = 2
+#   if (return_odds == true)
+#     i = 0
+#     s = string.split('')
+#     num = s + s[1]
+#   end
+# end
